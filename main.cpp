6037d883680738ae79e9a19c669707012018eb37
Aidan <aidanmichaelwoodcock@gmail.com>
2025-12-11 21:05:39 -0800

    update

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+// a project that simulates the queues associated with multiple vendor booths using a variety of data structures.
+
+int main() {
+
+
+
+    return 0;
+}
